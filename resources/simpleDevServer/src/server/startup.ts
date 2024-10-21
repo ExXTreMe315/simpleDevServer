6bd965e5a3fe7d8971956a63d5681f1f68e17adb
ExXTreMe315 <exxtreme@richman-gaming.de>
2024-10-21 14:21:58 +0200

    rew: (server/startup) spawn Player

--- resources/simpleDevServer/src/server/startup.ts
+++ resources/simpleDevServer/src/server/startup.ts
@@ -1,2 +1,7 @@
 import * as alt from 'alt-server'
 import * as shared from 'alt-shared'
+
+alt.on('playerConnect', (player: alt.Player) => {
+    player.spawn(0, 0, 72, 0)
+    player.model = 'mp_m_freemode_01'
+})
